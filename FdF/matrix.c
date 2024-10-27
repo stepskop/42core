ba428e580be50d656dcd5e6cfc88225ed3db46a6
stepskop <skopek.stepan@gmail.com>
2024-10-27 14:00:50 +0100

    chore(FdF): pass angle diractly to the Matrixes

--- FdF/matrix.c
+++ FdF/matrix.c
@@ -12,34 +12,28 @@
 
 #include "fdf.h"
 
-static void	rotate_x(int *y, int *z, t_fdf fdf)
+static void	rotate_x(int *y, int *z, double angle)
 {
 	int		tmp;
-	double	angle;
 
-	angle = fdf.camera.x_angle;
 	tmp = *y;
 	*y = *y * cos(angle) + *z * sin(angle);
 	*z = tmp * -sin(angle) + *z * cos(angle);
 }
 
-static void	rotate_y(int *x, int *z, t_fdf fdf)
+static void	rotate_y(int *x, int *z, double angle)
 {
 	int		tmp;
-	double	angle;
 
-	angle = fdf.camera.y_angle;
 	tmp = *x;
 	*x = *x * cos(angle) + *z * sin(angle);
 	*z = tmp * -sin(angle) + *z * cos(angle);
 }
 
-static void	rotate_z(int *x, int *y, t_fdf fdf)
+static void	rotate_z(int *x, int *y, double angle)
 {
 	int		tmp;
-	double	angle;
 
-	angle = fdf.camera.z_angle;
 	tmp = *x;
 	*x = *x * cos(angle) - *y * sin(angle);
 	*y = tmp * sin(angle) + *y * cos(angle);
@@ -53,9 +47,9 @@ t_point	get_rotation(t_point point, int z, t_fdf fdf)
 	point.x -= (fdf.map_w * fdf.camera.scale) / 2;
 	point.y -= (fdf.map_h * fdf.camera.scale) / 2;
 	mut_z = z;
-	rotate_x(&point.y, &mut_z, fdf);
-	rotate_y(&point.x, &mut_z, fdf);
-	rotate_z(&point.x, &point.y, fdf);
+	rotate_x(&point.y, &mut_z, fdf.camera.x_angle);
+	rotate_y(&point.x, &mut_z, fdf.camera.y_angle);
+	rotate_z(&point.x, &point.y, fdf.camera.z_angle);
 	point.y += HEIGHT / 2 + fdf.camera.offset.y;
 	point.x += WIDTH / 2 + fdf.camera.offset.x;
 	return (point);
